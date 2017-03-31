d962f8cfc3eb4f7181b138f2270954b0e7ed1800
Arif Rahman <xossarif@gmail.com>
2017-03-31 19:52:23 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($){
+	$(".greencircle").after('<img src="img/greencircle.png" alt="" class="greencirclemain"/>');
+	$(".red_circle").after('<img src="img/red_circle.png" alt="" class="red_circlemain"/>');
+	$(".orange_circle").after('<img src="img/orange_circle.png" alt="" class="orange_circlemain"/>');
+	$(".ash_circle").after('<img src="img/ash_circle.png" alt="" class="ash_circlemain"/>');
+});
